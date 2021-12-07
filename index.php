1f834a53f3f06aa0cc2351bc2db1f806720b08ed
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-12-08 01:21:58 +0330

    Get update and response for each messages

--- index.php
+++ index.php
@@ -1,18 +1,40 @@
 <?php
 define('TOKEN', '2105225490:AAFPOMU7VtNm77hRpwQcdKO5GlQXIhJpDTg');
+define('UPDATE_ID_FILE', __DIR__ . '/update_id.txt');
 
 $url = 'https://api.telegram.org/bot' . TOKEN . '/';
 
 $admin_id = '950263421';
 
-$get_me = $url . 'getMe';
+// $SayHello = $url . 'sendMessage?text=Hi&chat_id=' . $admin_id;
 
-$SayHello = $url . 'sendMessage?text=Hi&chat_id=' . $admin_id;
+// var_dump($get_update);
+$file = fopen(UPDATE_ID_FILE, 'r');
 
-var_dump($SayHello);
+$last_message_id = fread($file, filesize(UPDATE_ID_FILE));
+fclose($file);
+$get_update = $url . 'getUpdates?offset=' . $last_message_id;
 
-$curl = curl_init($SayHello);
+$curl = curl_init($get_update);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-$result = curl_exec($curl);
+$update_result = curl_exec($curl);
 
-var_dump($result);
+$messages = json_decode($update_result, true);
+$has_message = false;
+foreach ($messages['result'] as $message) {
+    $chat_id = $message['message']['chat']['id'];
+    $text = 'ممنونم که به گفتی ' . $message['message']['text'];
+
+    $say_thank = $url . 'sendMessage?text=' . $text . '&chat_id=' . $chat_id;
+
+    $curl = curl_init($say_thank);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $update_result = curl_exec($curl);
+
+    $last_message_id = $message['update_id'];
+    $has_message = true;
+}
+$file = fopen(UPDATE_ID_FILE, 'w+');
+
+fwrite($file, $last_message_id + intval($has_message));
+fclose($file);
