e61a87684beea15f28c3e132d4ca5abca7849a6d
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-12-31 13:59:22 +0330

    use controller here

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <?php
 include __DIR__ . '/env.php';
 include __DIR__ . '/TelegramLib.php';
+include __DIR__ . '/Controller.php';
 
-$messages = TelegraLib::get_update();
+$updates = TelegraLib::get_update();
 
 
 $last_message_id = 0;
-foreach ($messages as $message) {
-    $chat_id = $message['message']['chat']['id'];
-    $text = 'ممنونم که به گفتی ' . $message['message']['text'];
+foreach ($updates as $update) {
 
-    TelegraLib::send_message($text, $chat_id);
+    $controller = new Controller();
 
-    $last_message_id = $message['update_id'];
+    $controller->handle($update);
+
+    $last_message_id = $update['update_id'];
 }
-$messages = TelegraLib::get_update($last_message_id + 1);
+$updates = TelegraLib::get_update($last_message_id + 1);
