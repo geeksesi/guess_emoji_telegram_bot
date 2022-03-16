f8d8c1c682e3beb6f5eaaa2a850df052354e80e1
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2022-03-16 04:21:00 +0330

    add charset to phinx config

--- phinx.php
+++ phinx.php
@@ -4,22 +4,23 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
 return [
-    'paths' => [
-        'migrations' => '%%PHINX_CONFIG_DIR%%/db/migrations',
-        'seeds' => '%%PHINX_CONFIG_DIR%%/db/seeds',
+    "paths" => [
+        "migrations" => "%%PHINX_CONFIG_DIR%%/db/migrations",
+        "seeds" => "%%PHINX_CONFIG_DIR%%/db/seeds",
     ],
-    'environments' => [
-        'default_migration_table' => 'phinxlog',
-        'default_environment' => 'production',
-        'production' => [
-            'adapter' => 'mysql',
-            'host' => $_ENV['MYSQL_HOST'],
-            'name' => $_ENV['MYSQL_DB'],
-            'user' => $_ENV['MYSQL_USERNAME'],
-            'pass' => $_ENV['MYSQL_PASSWORD'],
-            'port' => $_ENV['MYSQL_PORT'],
-            'charset' => 'utf8',
+    "environments" => [
+        "default_migration_table" => "phinxlog",
+        "default_environment" => "production",
+        "production" => [
+            "adapter" => "mysql",
+            "host" => $_ENV["MYSQL_HOST"],
+            "name" => $_ENV["MYSQL_DB"],
+            "user" => $_ENV["MYSQL_USERNAME"],
+            "pass" => $_ENV["MYSQL_PASSWORD"],
+            "port" => $_ENV["MYSQL_PORT"],
+            "charset" => "utf8",
+            "collation" => "utf8_unicode_ci",
         ],
     ],
-    'version_order' => 'creation',
+    "version_order" => "creation",
 ];
