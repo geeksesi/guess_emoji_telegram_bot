44d5cc27a915bc926ab1c1bcb600085780631e0c
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2022-03-17 00:24:46 +0330

    generate auto hint need more work to do

--- app/Helper/LevelHintsHelper.php
+++ app/Helper/LevelHintsHelper.php
@@ -3,7 +3,7 @@
 namespace App\Helper;
 
 use App\Model\Level;
-use LevelHintsTypeEnum;
+use App\Enums\LevelHintsTypeEnum;
 
 class LevelHintsHelper
 {
@@ -13,9 +13,11 @@ class LevelHintsHelper
         $all_hidden = self::word_place($level->answer);
         shuffle($range);
         $phase_count = (int) (count($range) / 4);
-        $phase_one = array_chunk($range, $phase_count);
-        $phase_two = array_chunk($range, $phase_count * 2);
-        $phase_three = array_chunk($range, $phase_count * 3);
+        $chunks = array_chunk($range, $phase_count);
+        $phase_one = $chunks[0];
+        $phase_two = array_merge($phase_one, $chunks[1]);
+        $phase_three = array_merge($phase_two, $chunks[2]);
+
         $output = [
             1 => [
                 "level_id" => $level->id,
@@ -66,17 +68,20 @@ class LevelHintsHelper
 
     private static function word_phase(string $_answer, array $_keys): string
     {
-        $output = [];
+        $output = "";
         foreach (str_split($_answer) as $i => $c) {
+            // var_dump($i . "::" . $_answer[$i]);
             if (in_array($i, $_keys)) {
-                $output[] = $c;
-            }
-            if ($c == " ") {
-                $output[] = " ";
+                $output .= $_answer[$i];
                 continue;
             }
-            $output[] = "*";
+            if ($c == " ") {
+                $output .= " ";
+                continue;
+            }
+            $output .= "*";
         }
-        return implode("", $output);
+        // var_dump(implode("", $output), count($_keys));
+        return mb_convert_encoding($output, "UTF-8", "UTF-8");
     }
 }
