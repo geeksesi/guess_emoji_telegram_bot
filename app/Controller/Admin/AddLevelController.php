8500c03a4d6def67c1f34ea3eb33e35c526c9a2d
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2022-03-16 04:23:04 +0330

    WIP #1

--- app/Controller/Admin/AddLevelController.php
+++ app/Controller/Admin/AddLevelController.php
@@ -3,12 +3,42 @@
 namespace App\Controller\Admin;
 
 use App\Controller\Controller;
+use App\Helper\LevelHintsHelper;
 use App\Helper\TelegramHelper;
+use App\Model\Level;
 
 class AddLevelController extends Controller
 {
+    public function error(string $_text)
+    {
+        TelegramHelper::send_message($_text, $this->chat_id);
+        return false;
+    }
     public function __invoke()
     {
-        TelegramHelper::send_message("HELLOa", $this->update["message"]["chat"]["id"]);
+        $lines = explode("\n", $this->update["message"]["text"]);
+
+        foreach ($lines as $line) {
+            $this->handle_line($line);
+        }
+        // json_encode($this->update, JSON_PRETTY_PRINT)
+    }
+
+    public function handle_line(string $line)
+    {
+        $sections = explode("-", $line);
+        if (!isset($sections[1])) {
+            return;
+        }
+
+        $question = $sections[0] ?? $this->error("Unvalid quest");
+        $answer = $sections[1] ?? $this->error("Unvalid answer");
+        $order = (int) $sections[2] ?? Level::get_last_order();
+
+        $level = Level::create(["quest" => $question, "answer" => $answer, "orders" => $order]);
+        $auto_hints = LevelHintsHelper::generate($level);
+
+        TelegramHelper::send_message($line, json_encode($level, JSON_UNESCAPED_UNICODE & JSON_PRETTY_PRINT));
+        TelegramHelper::send_message($line, json_encode($auto_hints, JSON_UNESCAPED_UNICODE & JSON_PRETTY_PRINT));
     }
 }
