1a1b760278abd33c55951a7c11c4291061fd2b2c
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2022-03-31 14:26:05 +0430

    feat: #17 buy credit keyboard

--- app/Controller/Keyboard/BuyCreditKeyboardController.php
+++ app/Controller/Keyboard/BuyCreditKeyboardController.php
@@ -3,26 +3,33 @@
 namespace App\Controller\Keyboard;
 
 use App\Controller\Controller;
-use App\Enums\OutputMessageEnum;
-use App\Helper\OutputHelper;
 use App\Helper\TelegramHelper;
+use App\Model\Plan;
 
 class BuyCreditKeyboardController extends Controller
 {
     public function __invoke(): bool
     {
         // Show plans
-
-        $keyboard["inline_keyboard"] = [
-            [
+        $plans = Plan::get_all("WHERE status=:status", [":status" => 1], "ORDER BY credit ASC");
+        foreach ($plans as $plan) {
+            $message = "بسته : " . $plan->name;
+            $message .= "\n";
+            $message .= $plan->description;
+            $keyboard = [];
+            $keyboard["inline_keyboard"] = [
                 [
-                    "text" => "مشاهده آگهی",
-                    "url" => $_url,
-                ],
-            ],
-        ];
+                    [
+                        "text" => "خرید " . $plan->credit . " سکه با " . $plan->cost . " تومان",
 
-        TelegramHelper::send_message("در دست احداث", $this->chat_id);
+                        "url" => $_ENV["HTTP_URL"] . "/payment.php?key=" . $this->chat_id . "&plan=" . $plan->id,
+                    ],
+                ],
+            ];
+
+            TelegramHelper::send_message($message, $this->chat_id, $keyboard);
+        }
+
         return true;
     }
 }
