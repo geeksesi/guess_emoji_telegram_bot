c7d246e41fbd8c3510769ef56bd053739f70b580
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2022-04-10 10:22:54 +0430

    Update TelegramHelper.php

--- app/Helper/TelegramHelper.php
+++ app/Helper/TelegramHelper.php
@@ -49,15 +49,22 @@ class TelegramHelper
         }
         curl_close($curl);
         if (!is_null($error)) {
-            throw new \Exception("CURL :" . curl_error($curl));
+            (new ExceptionHepler(new \Exception("CURL :" . curl_error($curl))))();
+            return false ;
         }
 
         $output = json_decode($result, true);
         if (is_null($output)) {
-            throw new \Exception("CURL : EMPTY RESPONSE");
+            $exp = new \Exception("CURL : EMPTY RESPONSE");
+            (new ExceptionHepler($exp))();
+            return false ;
+            
         }
         if (!isset($output["ok"]) || !$output["ok"]) {
-            throw new \Exception("TG : " . json_encode($output["description"]));
+            $exp =  new \Exception("TG : " . json_encode($output["description"]));
+            (new ExceptionHepler($exp))();
+            return false ;
+            
         }
         return $output;
     }
